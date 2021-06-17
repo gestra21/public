8b9ef8b15532730b9688dcc11db102cb8ca54b03
gestra21 <80720446+gestra21@users.noreply.github.com>
2021-06-18 01:48:42 +0200

    Add files via upload

---
+++ acc.js
@@ -0,0 +1,17 @@
+var accountData={
+broker:"Swissquote Bank SA",
+DepositCurrency:"CHF",
+ClientName:"Neil Filippelli",
+server:"Swissquote-Server",
+accountLogin:5160966,
+accountLeverage:100,
+accountBalance:1005.07,
+accountCredit:0.0,
+accountProfit:-44.68,
+accountEquity:960.39,
+accountMargin:992.87,
+accountMarginFree:-32.48,
+accountMarginLevel:96.73,
+accountMarginSoCall:100.0,
+accountMarginSoSo:30.0
+};
